22a5f00d2bcda869a86683773398eb72822e727f
Maciej Pędzich (Mac) <contact@maciejpedzi.ch>
2022-11-27 18:51:34 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,48 @@
 <script setup>
 import { ref, watch } from 'vue'
+import { debounce } from 'debounce'
 
+const isLoading = ref(false)
 const searchTerm = ref('')
+const products = ref([])
 
-const findProducts = async term => {}
+const findProducts = debounce(async term => {
+  try {
+    if (term.length === 0) return (products.value = [])
 
-watch(searchTerm, newTerm => findProducts(newTerm))
+    isLoading.value = true
+
+    const apiResponseBody = await (
+      await fetch(`https://dummyjson.com/products/search?q=${term}&limit=5`)
+    ).json()
+
+    products.value = apiResponseBody.products
+  } catch (error) {
+    console.error(error)
+    alert('Something went wrong!')
+  } finally {
+    isLoading.value = false
+  }
+}, 300)
+
+watch(searchTerm, newSearchTerm => findProducts(newSearchTerm))
 </script>
 
 <template>
   <div class="w-full h-full flex flex-col gap-5 justify-center items-center">
     <h1 class="text-4xl font-bold">Gift Search Bar</h1>
-    <input type="text" class="p-2 border-2 border-gray-dark" v-model="searchTerm" placeholder="Start typing..." />
-    <ul class="list-disc">
-      <li>Display suggestions here</li>
+    <input
+      type="text"
+      class="p-2 border-2 border-gray-dark disabled:opacity-40"
+      v-model="searchTerm"
+      placeholder="Start typing..."
+      :disabled="isLoading"
+    />
+    <p v-if="isLoading" class="text-lg text-center">Please wait...</p>
+    <ul v-else-if="!loading && products.length > 0" class="list-disc">
+      <li v-for="product in products" :key="product.id">
+        {{ product.title }} - ${{ product.price }}
+      </li>
     </ul>
   </div>
 </template>
